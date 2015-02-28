c8f2b8591d404b87ef7e40dc5d06a84a68f5a962
Clint Moore <clint@ivy.io>
2015-02-27 23:46:49 -0800

    99 red balloons.

--- balloons.lisp
+++ balloons.lisp
@@ -33,10 +33,10 @@
 (defmacro+ps _ (function &rest body)
   `(-> _ (,function ,@body)))
 
-(defmacro+ps add-entity (texture)
+(defmacro+ps add-entity ()
   (with-ps-gensyms (ball)
     `(progn
-       (defvar ,ball (chain game add (sprite 0 0 ,texture)))
+       (defvar ,ball (chain game add (sprite 0 0 "balloon")))
 
        (chain game physics (enable ,ball (chain -phaser -physics -a-r-c-a-d-e)))
        (setf (@ ,ball check-world-bounds) t)
@@ -47,8 +47,6 @@
        
        (setf (chain ,ball body velocity y) (random-range -20 -50))
 
-       (setf (-> ,ball itexture) ,texture)
-       
        (chain all-balloons (push ,ball))
        (drop-balloon ,ball))))
 
@@ -68,10 +66,7 @@
     (defun drop-balloon (the-balloon)
       (setf (@ the-balloon position x) (random-range 1 window-width))
       (setf (@ the-balloon position y) (random-range window-height 2000))
-      (when (string= (@ the-balloon itexture) "balloon")
-        (-> the-balloon scale (set 0.5)))
-       (when (string= (@ the-balloon itexture) "roo")
-         (-> the-balloon scale (set .75))))
+      (-> the-balloon scale (set 0.5)))
     
     (defvar all-balloons (new (-array)))
 
@@ -79,13 +74,7 @@
       (setf score (+ score 1))
       (update-score)
       (drop-balloon obj)
-
-      (when (= (-> obj itexture) "roo")
-        (-> boingfx (play "boing")))
-      (when (= (-> obj itexture) "daisy")
-        (-> boingfx (play "boing")))
-      (when (= (-> obj itexture) "balloon")
-        (-> popfx (play "pop"))))
+      (-> popfx (play "pop")))
     
     (defun handle-collision (first second)
       (setf (-> first body velocity x) 10)
@@ -121,14 +110,8 @@
     
     (defun preload ()
       (-> game load (image "balloon" "img/balloon_red.png"))
-      (-> game load (image "balloon-clicked" "img/balloon_red_clicked.png"))
       (-> game load (image "background" "img/background.jpg"))
-      (-> game load (image "daisy" "img/daisy.png"))
-      (-> game load (image "roo" "img/roo.png"))
-      
-      (-> game load (audio "popfx" "snd/pop.ogg"))
-      (-> game load (audio "boingfx" "snd/boing.ogg"))
-      (-> game load (audio "sheepfx" "snd/sheep.ogg")))
+      (-> game load (audio "popfx" "snd/pop.ogg")))
         
     (defun create ()
       (setf (-> game stage disable-visibility-change) t)
@@ -142,25 +125,13 @@
                                                             stroke-thickness 2
                                                             align "center"))))
       (-> score-text anchor (set-to 0.5 0.5))
-
       (setf popfx (-> game add (audio "popfx")))
-      (setf sheepfx (-> game add (audio "sheepfx")))
-      (setf boingfx (-> game add (audio "boingfx")))
-      
       (-> popfx (add-marker "pop" 0 100))
-      (-> sheepfx (add-marker "sheep" 0 100))
-      (-> boingfx (add-marker "boing" 0 100))
-      
+           
       (setf (-> popfx allow-multiple) t)
-      (setf (-> sheepfx allow-multiple) nil)
-      (setf (-> boingfx allow-multiple) nil)
       
-      (dotimes (i 50)
-        (add-entity "balloon"))
-      (dotimes (i 25)
-        (add-entity "daisy"))
-      (dotimes (i 25)
-        (add-entity "roo")))))
+      (dotimes (i 99)
+        (add-entity)))))
 
 (define-easy-handler (serve-js :uri "/balloons.js") ()
   (setf (content-type*) "text/javascript")
